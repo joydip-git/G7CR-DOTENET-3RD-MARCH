870dbe5535fe9d861aaf7a7a4c6d84c2cf0ff8fc
joydip-git <joydip.mondal@gmail.com>
2026-03-03 14:04:57 +0530

    task uploaded

---
+++ codes/day-1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using MessengerLibrary;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Messenger messenger = new Messenger();
+            string message = messenger.Greet("Joydip");
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ codes/day-1/HelloWorld/MessengerLibrary/Messenger.cs
@@ -0,0 +1,10 @@
+﻿namespace MessengerLibrary
+{
+    public class Messenger
+    {
+        public string Greet(string name)
+        {
+            return $"Hello, {name}!";
+        }
+    }
+}
