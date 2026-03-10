af524ee15d2f81615839c9804d34d0d13ec3d07c
joydip-git <joydip.mondal@gmail.com>
2026-03-10 14:59:08 +0530

    key value pair collections and task uploaded

---
+++ codes/day-6/KeyValueCollections/KeyValueCollections/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace KeyValueCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> preferences = new();
+            SortedList<string, string> preferences = new();
+
+            preferences.Add("joydip", "Bank Account");
+            preferences.Add("harika", "Investment");
+            
+
+            //will throw error because of the duplicate key
+            //preferences.Add("joydip", "Funds Transfer");
+
+            //will update the value associated with key if the key exists
+            //will add the key and the value if the key does not exist
+            preferences["joydip"] = "Funds Transfer";
+            //preferences.Remove("joydip");
+
+            foreach (KeyValuePair<string,string> kvPair in preferences)
+            {
+                Console.WriteLine($"{kvPair.Key}: {kvPair.Value}");
+            }
+
+            if (preferences.ContainsKey("kaif"))
+            {
+                Console.WriteLine("exists");
+            }
+        }
+    }
+}
