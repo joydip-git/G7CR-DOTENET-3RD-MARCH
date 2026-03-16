4649bdf534f3507642b28778a9c22ce3a93e3d9d
joydip-git <joydip.mondal@gmail.com>
2026-03-16 11:23:44 +0530

    association and LINQ - group join

---
+++ codes/day-10/HasARelationship/HasARelationship/Program.cs
@@ -0,0 +1,98 @@
+﻿Category laptop = new() { CategoryId = 1, CategoryName = "Laptop" };
+Category mobile = new() { CategoryId = 2, CategoryName = "Mobile" };
+Category book = new() { CategoryId = 3, CategoryName = "Book" };
+
+
+Product dellLaptop = new() { ProductId = 1, Price = 120000.00M, ProductName = "Dell Xps 15", Category = laptop, CategoryId = laptop.CategoryId };
+
+Product onePlusMobile = new() { ProductId = 2, Price = 50000.00M, ProductName = "One PLus 13", Category = mobile, CategoryId = mobile.CategoryId };
+
+Product alchemistBook = new() { ProductId = 3, Price = 700.00M, ProductName = "The Alchemist", Category = book, CategoryId = book.CategoryId };
+
+Console.WriteLine(dellLaptop.Category.CategoryName);
+
+//if the Products property is assigned to a collection already
+laptop.Products.Add(dellLaptop);
+mobile.Products.Add(onePlusMobile);
+book.Products.Add(alchemistBook);
+
+//if the Products property is null
+//laptop.Products = [dellLaptop];
+//mobile.Products = [onePlusMobile];
+//book.Products = [alchemistBook];
+
+//individual book records
+Book book1 = new() { BookIsbn = 100, Price = 1000, Title = "The Alchemist" };
+Book book2 = new() { BookIsbn = 201, Price = 899, Title = "some book" };
+//books table
+var books = new List<Book> { book1, book2 };
+
+//individual customer records
+Customer anilCust = new() { CustomerId = 1, CustomerName = "anil" };
+Customer sunilCust = new() { CustomerId = 2, CustomerName = "sunil" };
+//customers table
+var cutsomers = new List<Customer> { anilCust, sunilCust };
+
+//individual entry records
+LedgerEntry entry1 = new() { EntryId = 1, BorrowDate = new DateTime(2026, 3, 7), Customer = anilCust, CustomerId = anilCust.CustomerId, Book = book1, BookIsbn = book1.BookIsbn };
+entry1.ReturnDate = DateTime.Now;
+
+LedgerEntry entry2 = new() { EntryId = 2, BorrowDate = new DateTime(2026, 3, 9), Customer = sunilCust, CustomerId = sunilCust.CustomerId, Book = book2, BookIsbn = book2.BookIsbn };
+//entries tables
+var entries = new List<LedgerEntry>() { entry1, entry2 };
+
+class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; } = string.Empty;
+
+    //public ICollection<Product>? Products { get; set; }
+    public ICollection<Product> Products { get; set; } = [];
+}
+
+class Product
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public decimal? Price { get; set; }
+    public int? CategoryId { get; set; }
+
+    //navigation property
+    public Category? Category { get; set; }
+}
+
+class Book
+{
+    public int BookIsbn { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public decimal? Price { get; set; }
+    public ICollection<Author>? Authors { get; set; }
+    //public ICollection<Customer>? Customers { get; set; }
+}
+
+class Author
+{
+    public long MobileNo { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Book>? Books { get; set; }
+}
+class Customer
+{
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    //public ICollection<Book>? Books { get; set; }
+}
+
+//Association class
+class LedgerEntry
+{
+    public int EntryId { get; set; }
+    public int CustomerId { get; set; }
+    public int BookIsbn { get; set; }
+    public DateTime BorrowDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+
+    //navigation properties
+    public Customer? Customer { get; set; }
+    public Book? Book { get; set; }
+}
---
+++ codes/day-10/LINQPractice/LINQPractice/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var customers = new List<Customer>
+{
+    new() { Id = 1, Name = "anil" },
+    new() { Id = 2, Name = "sunil" },
+    new() { Id = 3, Name = "joydip" }
+};
+
+var orders = new List<Order>
+{
+    new(){ Id=100, CustomerId=customers[0].Id, Customer=customers[0], Date=new DateTime(2026,3,10), Amount=1000 },
+    new(){ Id=101, CustomerId=customers[0].Id, Customer=customers[0], Date=new DateTime(2026,3,11), Amount=2000 },
+    new(){ Id=102, CustomerId=customers[1].Id, Customer=customers[1], Date=new DateTime(2026,3,10), Amount=1500 },
+    new(){ Id=103, CustomerId=customers[1].Id, Customer=customers[1], Date=new DateTime(2026,3,12), Amount=2500 }
+};
+//select isnull(sum(amount),0) from orders where date=
+var total = orders
+    .Where(o => o.Date == new DateTime(2026, 3, 10))
+    .Sum(o => o.Amount);
+Console.WriteLine("\n\n");
+
+var query = (from c in customers
+             join o in orders on c.Id equals o.CustomerId into coGroup
+             from g in coGroup.DefaultIfEmpty(new Order() { Customer = c, CustomerId = c.Id, Date = DateTime.MinValue, Id = 0 })
+             select new
+             {
+                 CustomerName = g.Customer?.Name,
+                 Total = coGroup.Sum(o1 => o1.Amount)
+             }).Distinct();
+
+var query1 =
+    customers
+    .GroupJoin(
+        orders,
+        c => c.Id,
+        o => o.CustomerId,
+        (Customer cust, IEnumerable<Order> orderGroup) => new
+        {
+            CustomerName = cust.Name,
+            Total = orderGroup.Sum(order => order.Amount)
+        });
+
+query1.ToList().ForEach(record => Console.WriteLine(record.CustomerName + ":" + record.Total));
+
+/*
+ * anil  3000
+ * sunil 4000
+ * joydip 0
+ */
+
+Console.WriteLine(total ?? 0);
+
+
+class Customer
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public ICollection<Order>? Orders { get; set; }
+}
+class Order
+{
+    public required int Id { get; set; }
+    public required DateTime Date { get; set; }
+    public decimal? Amount { get; set; }
+    public required int CustomerId { get; set; }
+    public required Customer Customer { get; set; }
+}
